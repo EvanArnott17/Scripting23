1eadd93ca238d34c62f6f593ad1df022e73aaea9
EvanArnott17 <149430093+EvanArnott17@users.noreply.github.com>
2023-11-26 14:33:03 -0700

    Add files via upload

---
+++ cityBuilderStart/js/build.js
@@ -0,0 +1,48 @@
+//console log just to test that your JS is linked properly
+console.log("working?");
+
+
+//create a variable for the number of buildings in the city and start it at zero
+var buildings = 0;
+//send a message to the page to inform the user of how many buildings there are in their city. This will happen right away when the page loads
+document.getElementById('city').innerHTML = "<p>You have " + buildings + " buildings in your city!</p>";
+
+//Functions -- 
+//Think of this as a group of instructions that will always run together
+//Create a function that will run when the first button is clicked
+//This gets trigger by the submit button in the form. This trigger is known as an EVENT! (Button click event)
+//It will:
+    //-- Print a message to teh console
+    //--Add one to the variable that is tracking the number of buildings
+    //---Print a message with the number of buildings
+    //---Place an image graphic into the other div
+function buildOne(){
+    console.log("Function Test")
+    buildings += 1;
+    document.getElementById('city').innerHTML = "<p>You have " + buildings + " buildings in your city!</p>";
+    document.getElementById('cityGraphic').innerHTML += "<img src='images/BuildingBlock1.png'>"
+}
+
+//Create a function for the second button that prints a message with the number of buildings and the graphic for the second building
+function buildTwo(){
+    console.log("Function Test")
+    buildings += 1;
+    document.getElementById('city').innerHTML = "<p>You have " + buildings + " buildings in your city!</p>";
+    document.getElementById('cityGraphic').innerHTML += "<img src='images/BuildingBlock2.png'>"
+}
+
+//Create a function for the third button that prints a message with the number of buildings and the graphic for the second building
+function buildThree(){
+    console.log("Function Test")
+    buildings += 1;
+    document.getElementById('city').innerHTML = "<p>You have " + buildings + " buildings in your city!</p>";
+    document.getElementById('cityGraphic').innerHTML += "<img src='images/BuildingBlock3.png'>"
+}
+
+//Create function for a fourth button that will print another building
+function buildFour(){
+    console.log("Function Test 4")
+    buildings += 1;
+    document.getElementById('city').innerHTML = "<p> You have " + buildings + " buildings in your city!</p>";
+    document.getElementById('cityGraphic').innerHTML += "<img src='images/BuildingBlock4.png'>"
+}
